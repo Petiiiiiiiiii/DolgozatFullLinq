fcec898314210d405ba3860307e3e5a7901cbdf4
Orosz Péter <70895314+Petiiiiiiiiii@users.noreply.github.com>
2023-11-26 12:56:50 +0100

    Add files via upload

---
+++ Laptop.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA231126
+{
+    class Laptop
+    {
+        public int Azonosito { get; set; }
+        public string ProcesszorString { get; set; }
+        public double ProcesszorSeb { get; set; }
+        public string Oprendszer { get; set; }
+        public string GyartoModellString { get; set; }
+        public string Gyarto { get; set; }
+        public double Aksi { get; set; }
+        public double suly { get; set; }
+        public string VezetekNelkuliString { get; set; }
+        public List<string> VezetékNelkuli { get; set; }
+
+        public Laptop(string sor)
+        {
+            var atmeneti = sor.Split('|');
+            this.Azonosito = Convert.ToInt32(atmeneti[0]);
+            this.ProcesszorString = atmeneti[1];
+            var processzorSeb = atmeneti[1].Split(' ');
+            this.ProcesszorSeb = Convert.ToDouble(processzorSeb[processzorSeb.Length - 1]);
+            this.Oprendszer = atmeneti[2];
+            this.GyartoModellString = atmeneti[3];
+            var gyartoModell = atmeneti[3].Split(' ');
+            this.Gyarto = gyartoModell[0];
+            this.Aksi = Convert.ToDouble(atmeneti[4]);
+            this.suly = Convert.ToDouble(atmeneti[5]);
+            this.VezetekNelkuliString = atmeneti[6];
+            var vezetekNelkuli = atmeneti[6].Split(',');
+            this.VezetékNelkuli = new();
+            for (int i = 0; i < vezetekNelkuli.Length; i++)
+            {
+                this.VezetékNelkuli.Add(vezetekNelkuli[i]);
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Azonosító : {this.Azonosito} \nProcesszor típusa és sebessége: {this.ProcesszorString} \nOperációs rendszer: {this.Oprendszer} \nGyártó és modell: {this.GyartoModellString}\nAkkumulátor élettartama: {this.Aksi} \nSúlya: {this.suly} kg \nVezeték nélküli kapcsolatok: {this.VezetekNelkuliString}\n"; 
+        }
+
+        public double kgToGramm() 
+        {
+            return suly * 1000;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿using CA231126;
+
+static List<Laptop> Feladat7(List<Laptop> laptopok) 
+{
+    return laptopok
+        .Where(l => l.Oprendszer.StartsWith("Windows") == false && l.suly >= 2)
+        .ToList();
+}
+static void Feladat8(List<Laptop> laptopok) 
+{
+    var gyengeAksi = laptopok
+        .Where(l => l.Aksi == laptopok.Min(k => k.Aksi))
+        .ToList();
+
+    gyengeAksi.ForEach(x => Console.WriteLine(x));
+    Console.WriteLine($"{gyengeAksi.Count} db ilyen laptop van, legkevesebb üzeimdő pontosan: {gyengeAksi[0].Aksi}");
+}
+static double Feladat9(List<Laptop> laptopok) 
+{
+    return laptopok.Average(l => l.ProcesszorSeb);
+}
+static List<string> Feladat10(List<Laptop> laptopok)
+{
+    var Bluetooth5pont2 = laptopok
+    .SelectMany(l => l.VezetékNelkuli, (a, k) => new { Gyarto = a.Gyarto, Kapcsolat = k })
+    .Where(k => k.Kapcsolat == " Bluetooth 5.2")
+    .DistinctBy(a => a.Gyarto)
+    .OrderBy(a => a.Gyarto)
+    .Select(a => a.Gyarto)
+    .ToList();
+
+    return Bluetooth5pont2;
+}
+static List<int> Feladat11(List<Laptop> laptopok) 
+{
+    return laptopok
+        .Where(l => l.ProcesszorString.StartsWith("AMD") && l.suly > 1.3)
+        .Select(l => l.Azonosito)
+        .ToList();
+}
+static void Kiiras(List<Laptop> laptopok) 
+{
+    StreamWriter sw = new(@"..\..\..\src\laptopokUj.txt");
+    laptopok.ForEach(l => sw.WriteLine($"Processzor típusa és sebessége: {l.ProcesszorString}, Súlya: {l.kgToGramm()} gramm"));
+    sw.Close();
+}
+
+List<Laptop> laptopok = new();
+StreamReader sr = new(@"..\..\..\src\laptopok.txt");
+
+_ = sr.ReadLine();
+while (!sr.EndOfStream)
+{
+    laptopok.Add(new Laptop(sr.ReadLine()));
+}
+
+laptopok.ForEach(x => Console.WriteLine(x));
+
+Console.WriteLine("7. feladat:");
+List<Laptop> Feladat7List = Feladat7(laptopok);
+Console.WriteLine($"\t{Feladat7List.Count} db ilyen van");
+
+Console.WriteLine("\n8. feladat:\n");
+Feladat8(laptopok);
+
+Console.WriteLine("\n9. feladat:");
+Console.WriteLine($"\t{Feladat9(laptopok)} az átlagos processzor sebesség");
+
+Console.WriteLine("\n10. feladat:");
+List<string> feladat10 = Feladat10(laptopok);
+try
+{
+    feladat10.ForEach(x => Console.WriteLine($"\t {x}"));
+}
+catch
+{
+    Console.WriteLine("\tNincsen a keresettnek megfelelő laptop!");
+}
+
+Console.WriteLine("\n11. feladat:");
+Console.Write("\tAzonosítók: ");
+Feladat11(laptopok).ForEach(l => Console.Write($"{l}, "));
+
+Console.WriteLine("\n13. feladat:");
+try
+{
+    Kiiras(laptopok);
+    Console.WriteLine("\tFájl elkészült!");
+}
+catch
+{
+    Console.WriteLine("Hiba a fájlba kiírás során!");
+}
+
+Console.ReadKey();
